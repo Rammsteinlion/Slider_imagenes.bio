bfd4ccdcba051bd6cb59d74ef6645e29e8dbadf5
Elkin <rastaw28@gmail.com>
2021-05-12 21:13:16 -0500

    carrusel de imagenes con jquery

---
+++ contenido.php
@@ -0,0 +1,30 @@
+<?php 
+
+function Contenido(){
+    $datos= array();
+
+              $datos[0] ="img/ballena.jpg"."||".
+               "Ballena"."||".               
+               "Las ballenas son mamíferos acuáticos, de hecho son los mamíferos de mayor tamaño.
+                Pertenecen al orden de los cetáceos y se distribuyen por todos los océanos del mundo";
+
+               $datos[1] ="img/Pez_Payaso.jpg"."||".
+               "Pez Payaso"."||".               
+               "El pez payaso es un pequeño pez marino, que forma parte del paisaje de los arrecifes de coral del océano Indo-Pacífico.";
+
+               $datos[2] ="img/Serpiente.jpg"."||".
+               "Serpiente"."||".               
+               " caracterizada por un cuerpo alargado, está preparada para permitir un desplazamiento sin necesidad de extremidades: pueden moverse cómodamente tanto por tierra como por agua.";
+
+               $datos[3] ="img/ornitorrinco-pico.png"."||".
+               "Ornitorrinco"."||".               
+               "El cuerpo del ornitorrinco es delgado y alargado, de forma aerodinámica. ... En el macho, sobresale en las patas traseras un espolón conectado por un conducto al veneno de la glándula";
+
+               $datos[3] ="img/jirafa.jpg"."||".
+               "Jirafa"."||".               
+               "Este mamífero del orden de los Artiodactyla, es un animal ungulado que posee un par de dedos en las extremidades";
+
+               return $datos;
+}
+
+
